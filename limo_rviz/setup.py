5d492d049b40293754f85c90f552b0a76020f685
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-10-31 03:52:11 +0000

    Update setup.py

--- limo_rviz/setup.py
+++ limo_rviz/setup.py
@@ -12,19 +12,11 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        # (
-        #     os.path.join("share", package_name, "launch"),
-        #     glob("launch/*launch.[pxy][yma]*"),
-        # ),
-        (os.path.join("share", package_name, "rviz"), glob("rviz/*.rviz")),
         (
             os.path.join("share", package_name, "launch"),
-            [
-                filepath
-                for filepath in glob("launch/*", recursive=True)
-                if os.path.isfile(filepath)
-            ],
+            glob("launch/*launch.[pxy][yma]*"),
         ),
+        (os.path.join("share", package_name, "rviz"), glob("rviz/*.rviz")),
         (
             os.path.join("share", package_name, "models", "turtlebot3_square"),
             [
